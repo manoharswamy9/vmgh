46ce9a9812331cba59c06cc66166198f5e8f2548
manohar <smanohar4321@gmail.com>
2020-06-27 21:06:45 +0530

    first commit

---
+++ src/com/javaguides/config/AppInitializer.java
@@ -0,0 +1,28 @@
+package com.javaguides.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		 return new Class[] {
+		            AppContext.class
+		        };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {
+                WebMvc.class
+            };
+
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		 return new String[] {
+		            "/"
+		        };
+}
+}
